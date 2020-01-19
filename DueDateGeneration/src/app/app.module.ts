30d259b8a5f00e6cf332071fbdb3e5938ed42ee1
NityaAgr <nityajsr24@gmail.com>
2020-01-19 22:40:58 +0530

    Added frequency

--- DueDateGeneration/src/app/app.module.ts
+++ DueDateGeneration/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FrequencyComponent } from './frequency/frequency.component';
+import { DailyComponent } from './frequency/daily/daily.component';
+import { WeeklyComponent } from './frequency/weekly/weekly.component';
+import { MonthlyComponent } from './frequency/monthly/monthly.component';
+import { YearlyComponent } from './frequency/yearly/yearly.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrequencyComponent,
+    DailyComponent,
+    WeeklyComponent,
+    MonthlyComponent,
+    YearlyComponent
   ],
   imports: [
     BrowserModule,
